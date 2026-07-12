f953bd155c044b17aff16e00c7e7ced1951d4007
mybox1407-art <mybox1407@gmail.com>
2026-07-12 16:58:35 +0400

    Update app.ts

--- trade_bot_binance/src/app.ts
+++ trade_bot_binance/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import healthRouter from './routes/health';
+import botRouter from './routes/bot';
 
 export const app = express();
 
 app.use(express.json());
 
 app.use('/health', healthRouter);
+app.use('/bot', botRouter);
