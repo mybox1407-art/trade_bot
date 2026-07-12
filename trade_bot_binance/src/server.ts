ba50261a1361a5770004a9d4a5d4b2a818b2108f
mybox1407-art <mybox1407@gmail.com>
2026-07-12 19:00:30 +0400

    Update server.ts

--- trade_bot_binance/src/server.ts
+++ trade_bot_binance/src/server.ts
@@ -1,6 +1,8 @@
 import { app } from './app';
 import { env } from './config/env';
+import { startScheduler } from './services/scheduler';
 
 app.listen(env.port, () => {
   console.log(`Server started on port ${env.port}`);
+  startScheduler();
 });
