9345a016f29d50c229b104056291d55b65790649
mybox1407-art <mybox1407@gmail.com>
2026-07-12 17:36:56 +0400

    Update botRunner.ts

--- trade_bot_binance/src/services/botRunner.ts
+++ trade_bot_binance/src/services/botRunner.ts
@@ -1,11 +1,18 @@
-import { getMockPrice } from './exchange';
+import { getCurrentPrice } from './exchange';
 import { shouldBuy } from './strategy';
 
 export async function runBotOnce() {
-  const price = await getMockPrice();
+  const symbol = 'BTC/USDT';
+  const price = await getCurrentPrice(symbol);
+
+  if (price === null) {
+    return { symbol, price: null, buy: false };
+  }
+
   const buy = shouldBuy(price);
 
   return {
+    symbol,
     price,
     buy
   };
